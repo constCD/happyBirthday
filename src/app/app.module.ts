cc219f7c7d55ac1f6592ff343830d4cfe108b73e
dicai <dicai@cisco.com>
2022-03-18 17:28:13 +0800

    add text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {StartComponent} from './module/start/start.component';
-import {AvatarComponent} from './module/avatar/avatar.component';
-import {HomeComponent} from './module/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StartComponent } from './module/start/start.component';
+import { AvatarComponent } from './module/avatar/avatar.component';
+import { HomeComponent } from './module/home/home.component';
+import { HappyWordingsComponent } from './module/happy-wordings/happy-wordings.component';
 
 @NgModule({
   declarations: [
@@ -13,13 +14,10 @@ import {HomeComponent} from './module/home/home.component';
     StartComponent,
     AvatarComponent,
     HomeComponent,
-
+    HappyWordingsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
